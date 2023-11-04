765c7ca7b3eee18d2cb3da817dffa2b37d5edce0
C0Nd3Mnd <git@orangecarbon.de>
2023-11-04 15:27:58 +0100

    Create Firefox build separately from Chrome build

--- postbuild.js
+++ postbuild.js
@@ -1,5 +1,5 @@
-import pkg from "./package.json" assert { type: "json" };
 import { promises as fs } from "fs";
+import pkg from "./package.json" assert { type: "json" };
 
 async function postbuild() {
   const json = await fs.readFile("dist/manifest.json");
@@ -7,7 +7,31 @@ async function postbuild() {
 
   manifest.version = pkg.version;
 
-  await fs.writeFile("dist/manifest.json", JSON.stringify(manifest));
+  await fs.writeFile("dist/manifest.json", JSON.stringify(manifest, null, 2));
+
+  manifest.background = {
+    scripts: ["background.js"],
+  };
+
+  manifest.permissions.push("activeTab", "tabs");
+  manifest.browser_specific_settings = {
+    gecko: {
+      id: "forumlaenhanced@sleepingbird.dev",
+    },
+  };
+
+  try {
+    await fs.rm("dist-firefox", { recursive: true });
+  } finally {
+    await fs.mkdir("dist-firefox");
+  }
+
+  await fs.cp("dist", "dist-firefox", { recursive: true });
+
+  await fs.writeFile(
+    "dist-firefox/manifest.json",
+    JSON.stringify(manifest, null, 2),
+  );
 }
 
 postbuild().then(() => console.log("Postbuild done"));
