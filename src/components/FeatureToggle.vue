7b93f162ae448038f5afda929121a1f8afa56798
C0Nd3Mnd <git@orangecarbon.de>
2023-12-09 02:54:39 +0100

    Add ability to mark FeatureToggle as experimental

--- src/components/FeatureToggle.vue
+++ src/components/FeatureToggle.vue
@@ -9,18 +9,30 @@ const props = defineProps({
     type: String,
     required: true,
   },
+  experimental: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
 });
 </script>
 
 <template>
-  <v-list-item v-bind="$attrs" @click="addonStore.toggleItem(props.prop)">
+  <v-list-item
+    v-bind="$attrs"
+    @click="addonStore.toggleItem(props.prop)"
+    color="red"
+  >
     <template #prepend>
-      <v-list-item-action start>
+      <v-list-item-action class="d-flex flex-column">
         <v-checkbox-btn
           :model-value="addonStore.storage[props.prop]"
           color="primary"
         />
       </v-list-item-action>
     </template>
+    <v-list-item-media v-if="experimental" class="text-caption text-red">
+      Experimentell
+    </v-list-item-media>
   </v-list-item>
 </template>
